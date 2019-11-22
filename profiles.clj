a1ad8080a97e2432bddfe44d8dac662a152a667c
rafael <rafael_oliveira500@hotmail.com>
2019-11-21 22:40:47 -0300

    Removendo profile, pois nele conterá infos de produção

--- profiles.clj
+++
@@ -1,6 +0,0 @@
-{:dev-env-vars  {:env {:database-url  "postgres://bolha_musical_api_user:password1@127.0.0.1:5432/bolha_musical_api?stringtype=unspecified"
-                       :sendinblue-user-login    "You@Something.com"
-                       :sendinblue-user-password "sendinblue"
-                       :auth-key      "theSecretKeyUsedToCreateAndReadTokens"}}
- :test-env-vars {:env {:database-url  "postgres://bolha_musical_api_user:password1@127.0.0.1:5432/bolha_musical_api_test?stringtype=unspecified"
-                       :auth-key      "theSecretKeyUsedToCreateAndReadTokens"}}}
