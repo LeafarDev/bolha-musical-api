c7bac869256c6b566e48faa7db62b0212f18f372
rafael <rafael_oliveira500@hotmail.com>
2020-02-03 18:22:58 -0300

    Possibilidade de remover voto

--- src/bolha_musical_api/validations/votar_track_validation.clj
+++ src/bolha_musical_api/validations/votar_track_validation.clj
@@ -7,10 +7,12 @@
             [bolha-musical-api.util :refer [string-is-keyword?]]))
 
 (metis/defvalidator rule [:track_interno_id [:presence {:message ":field-required"}]]
-  [:cimavoto [:presence {:message ":field-required"}]
-   ['metis-bool? {:message ":field-must-be-boolean"}]]
-  [:refletir_spotify [:presence {:message ":field-required"}]
-   ['metis-bool? {:message ":field-must-be-boolean"}]])
+                    [:cimavoto [:presence {:message ":field-required"}
+                                :numericality {:only-integer true :is-not-an-int ":wrong-formatting"
+                                               :less-than    2 :is-not-less-than ":between -1 1"
+                                               :greater-than -2 :is-not-greater-than ":between -1 1"}]]
+                    [:refletir_spotify [:presence {:message ":field-required"}]
+                     ['metis-bool? {:message ":field-must-be-boolean"}]])
 
 (defn votar-track-playlist-validate
   "passa a validação para função de validação e tradução de erros"
